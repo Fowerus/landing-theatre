310601c0aab6818c72728f38455bb08bbacf2d35
Fowerus <fowerus@gmail.com>
2021-04-28 18:41:13 +0700

    v1.0

---
+++ script.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+	$("header button.open-menu").on('click', function(){
+		$('header button.open-menu').addClass('disable-button');
+		$('header nav button.close-button').removeClass('disable-button');
+
+		$('header aside.media-info').addClass('enable-menu-field');
+		$('header nav ul.navigation-list').addClass('enable-menu');
+	})
+
+
+	$('header nav button.close-button').on('click',function(){
+		$('header nav button.close-button').addClass('disable-button');
+		$('header button.open-menu').removeClass('disable-button');
+		$('header aside.media-info').removeClass('enable-menu-field');
+		$('header nav ul.navigation-list').removeClass('enable-menu');
+	})
+})
